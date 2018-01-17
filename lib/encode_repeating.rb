030f26b47b93d220cdc2ca780c0cf26cc038adce
shrutivanw <shruti.dalvi@gmail.com>
2018-01-16 17:14:40 -0800

    Incorporating feedback
    - Making the readme file more readable and including link to wikipedia for RLE.
    - Moving nil and empty test cases to edge cases.
    - Fixing the non repeating characters test case
    - Making the method template raise NotImplementedError

--- lib/encode_repeating.rb
+++ lib/encode_repeating.rb
@@ -2,5 +2,5 @@
 # with a number representing the frequency. The replacement is done only if the
 # string length will get reduced by this process.
 def encode_repeating(my_string)
-  my_string << "NOT IMPLEMENTED"
+  raise NotImplementedError
 end
--- specs/encode_repeating_spec.rb
+++ specs/encode_repeating_spec.rb
@@ -4,39 +4,34 @@ require_relative '../lib/encode_repeating'
 
 describe "encode repeating" do
   describe "basic tests" do
-    describe "test 1" do
-      it "string with repeating characters" do
-        test_string = "aaabbbbbcccc"
+    it "string with repeating characters" do
+      test_string = "aaabbbbbcccc"
 
-        encode_repeating(test_string)
+      encode_repeating(test_string)
 
-        test_string.must_equal "a3b5c4"
-      end
+      test_string.must_equal "a3b5c4"
     end
 
-    describe "test 2" do
-      it "string with repeating characters, some characters with only one occurence" do
-        test_string = "xxxyttttgeee"
+    it "string with repeating characters, some characters with only one occurence" do
+      test_string = "xxxyttttgeee"
 
-        encode_repeating(test_string)
+      encode_repeating(test_string)
 
-        test_string.must_equal "x3yt4ge3"
-      end
+      test_string.must_equal "x3yt4ge3"
     end
 
-    describe "test 3" do
-      it "string with repeating characters, some characters with only one or two occurences" do
-        test_string = "ddbbfffgjjjj"
+    it "string with repeating characters, some characters with only one or two occurences" do
+      test_string = "ddbbfffgjjjj"
 
-        encode_repeating(test_string)
+      encode_repeating(test_string)
 
-        test_string.must_equal "ddbbf3gj4"
-      end
+      test_string.must_equal "ddbbf3gj4"
     end
   end
 
-  # if it's a string parameter, check for empty
-  describe "empty string input test" do
+  # check for edge cases
+  describe "edge cases" do
+    # if it's a string parameter, check for empty
     it "empty string remains as is" do
       test_string = ""
 
@@ -44,10 +39,8 @@ describe "encode repeating" do
 
       test_string.must_be_empty
     end
-  end
 
-  # if the parameter is an object, check for nil
-  describe "nil object input test" do
+    # if the parameter is an object, check for nil
     it "nil object remains as is" do
       test_string = nil
 
@@ -55,28 +48,21 @@ describe "encode repeating" do
 
       test_string.must_be_nil
     end
-  end
 
-  # check for edge cases
-  describe "edge cases" do
-    describe "edge case 1" do
-      it "string with no consecutive repeating characters" do
-        test_string = "at53fag"
+    it "string with no consecutive repeating characters" do
+      test_string = "aeiou"
 
-        encode_repeating(test_string)
+      encode_repeating(test_string)
 
-        test_string.must_equal "at53fag"
-      end
+      test_string.must_equal "aeiou"
     end
 
-    describe "edge case 2" do
-      it "string with repeating spaces" do
-        test_string = "hi    there"
+    it "string with repeating spaces" do
+      test_string = "hi    there"
 
-        encode_repeating(test_string)
+      encode_repeating(test_string)
 
-        test_string.must_equal "hi 4there"
-      end
+      test_string.must_equal "hi 4there"
     end
   end
 end
